2ba666363babff9519d2a8f1622256d5c1c262de
ArkRaga <53240640+ArkRaga@users.noreply.github.com>
2019-12-06 11:58:30 -0500

    Tabs are done

--- components/Tabs/index.js
+++ components/Tabs/index.js
@@ -7,3 +7,31 @@
 //
 //  The tab component should look like this:
 //    <div class="tab">topic here</div>
+
+axios
+  .get("https://lambda-times-backend.herokuapp.com/topics")
+  .then(res => {
+    console.log(res.data.topics);
+    res.data.topics.forEach(ele => {
+      const tabsHome = document.querySelector(".topics");
+      tabsHome.appendChild(makeTabs(ele));
+      console.log(ele);
+    });
+  })
+  .catch(err => {
+    console.log(err);
+  });
+
+function makeTabs(data) {
+  //----------create Elements----\\
+  const tab = document.createElement("div"),
+    cont = document.createElement("p");
+
+  //-----------append-----------\\
+  tab.appendChild(cont);
+  //----------add Classes-------\\
+  cont.classList.add("tab");
+  //---------add Data----------\\
+  cont.textContent = data;
+  return tab;
+}
